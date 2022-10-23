ae089839a32bf37ec9640d46b248bd245f5078eb
Afiq Hatta <afiqhatta@Afiqs-MacBook-Air.local>
2022-10-23 09:56:00 +0100

    some post request endpoints

--- parse_api.py
+++ parse_api.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -13,3 +15,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+class ChatParser(BaseModel):
+    filename: str
+    prompter: str
+    responder: str
+    text: Union[str, None]
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: ChatParser, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
