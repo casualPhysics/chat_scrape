6807369926629f0a2bb67bb2d8a433b99aea3c18
afiqhatta <afiqhatta.ah@gmail.com>
2022-10-23 15:12:55 +0100

    Split up some functions

--- whatsapp_parser.py
+++ whatsapp_parser.py
@@ -3,14 +3,13 @@ from collections import defaultdict
 import re
 import argparse
 import datetime
-import os
 from pathlib import Path
 
 
 def converter(
-    filepath: str,
-    prompter: str,
-    responder: str,
+        filepath: str,
+        prompter: str,
+        responder: str,
 ) -> pd.DataFrame:
     """
     Turn whatsapp chat data into a format
@@ -23,6 +22,12 @@ def converter(
 
     with open(filepath, 'r') as fp:
         text = fp.read()
+    result_dict = text_to_dictionary(text, prompter, responder)
+    df = pd.DataFrame.from_dict(result_dict).T[['prompt', 'completion']]
+    return df
+
+
+def text_to_dictionary(text, prompter, responder):
     text_list = text.split('\n[')[1:]
     result_dict, count, prev_author = defaultdict(dict), 0, ''
     for ix, line in enumerate(text_list):
@@ -37,18 +42,17 @@ def converter(
                 result_dict[count]['prompt'] = f"{prev}.{message}"
             else:
                 count += 1
-                result_dict[count].update({'prompt' : message})
+                result_dict[count].update({'prompt': message})
 
         elif author == responder:
             if author == prev_author:
                 prev = result_dict[count]['completion']
                 result_dict[count]['completion'] = f"{prev}.{message}"
             else:
-                result_dict[count].update({'completion' : message})
+                result_dict[count].update({'completion': message})
 
         prev_author = author
-    df = pd.DataFrame.from_dict(result_dict).T[['prompt', 'completion']]
-    return df
+    return result_dict
 
 
 def construct_output_directory(output_name):
@@ -60,12 +64,11 @@ def construct_default_filename(prompter, responder):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Process your whatsapp chat data.')
     parser.add_argument('path', type=str, help='Path to file')
     parser.add_argument('prompter', type=str, help='Name of Prompter')
     parser.add_argument('responder', type=str, help='Name of Responder')
-    parser.add_argument('-filename', type=str, help='Destination filename' )
+    parser.add_argument('-filename', type=str, help='Destination filename')
 
     args = parser.parse_args()
     path = args.path
@@ -75,5 +78,3 @@ if __name__ == "__main__":
 
     save_file = filename if filename else datetime.datetime.now()
     converter(path, prompter, responder).to_csv(f'output_{save_file}.csv')
-
-
